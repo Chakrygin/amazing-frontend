2e2ee8315f0e63eb94cc45b59a4a7b40f91bc842
Igor Chakrygin <igor@chakrygin.ru>
2024-06-09 13:15:55 +0300

    test

--- tests/DevBlogsScraper.test.ts
+++
@@ -1,7 +0,0 @@
-import { testScraper } from 'core/testing';
-
-import DevBlogsScraper from '../src/scrapers/DevBlogsScraper'
-
-test('DevBlogs / TypeScript', async () => {
-  await testScraper(() => new DevBlogsScraper('typescript'));
-});

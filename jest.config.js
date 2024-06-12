382ccb3486f595cbe4eeb905548e450c42cf321f
Igor Chakrygin <igor@chakrygin.ru>
2024-06-12 23:43:18 +0300

    Fix

--- jest.config.js
+++ jest.config.js
@@ -2,6 +2,9 @@
 export default {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  setupFiles: [
+    '<rootDir>/jest.setup.ts',
+  ],
   moduleNameMapper: {
     '^@core/(.*)$': '<rootDir>/core/src/$1',
     '^@src/(.*)$': '<rootDir>/src/$1',
